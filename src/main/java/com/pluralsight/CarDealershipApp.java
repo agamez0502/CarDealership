045bc8f10c714e9629915537bf593a64e8667bc5
Alondra Gamez <agamez@appdev.yearup.org>
2025-05-13 12:49:51 -0500

    adding files - also just moved file here, completed Phase 1 (Dealership and Vehicle Class) on Friday 05/09

---
+++ src/main/java/com/pluralsight/CarDealershipApp.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class CarDealershipApp {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class DealershipFileManager {
+
+    public Dealership getDealership() {
+        Dealership dealership = null;
+
+        try{
+        } catch {}
+    }
+
+}
